d4515c0da705235db5d8eaaeaf1497e42513b66f
Arseny <arseny@protonmail.com>
2023-03-30 21:23:35 +0600

    fix: output shape

--- yolov5/models/yolo.py
+++ yolov5/models/yolo.py
@@ -80,7 +80,8 @@ class Detect(nn.Module):
         z = []  # inference output
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
-            x[i] = x[i].sigmoid() 
+            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
         return x 
     
